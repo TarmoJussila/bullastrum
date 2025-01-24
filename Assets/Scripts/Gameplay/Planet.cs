5ae76fb6e47b3c0914b7999bb1e798d8987e60b5
Tarmo Jussila <tarmo.jussila@gmail.com>
2025-01-24 23:43:48 +0200

    Planet rotation

--- Assets/Scripts/Gameplay/Planet.cs
+++ Assets/Scripts/Gameplay/Planet.cs
@@ -5,5 +5,41 @@ namespace Bullastrum.Gameplay
     public class Planet : MonoBehaviour
     {
         public Vector3 CenterPoint { get { return transform.position; } }
+        
+        [SerializeField] private float _rotationSpeed = 100f;
+
+        private Vector3 _lastMousePosition;
+        private bool _isDragging = false;
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                _isDragging = true;
+                _lastMousePosition = Input.mousePosition;
+            }
+            else if (Input.GetMouseButtonUp(1))
+            {
+                _isDragging = false;
+            }
+
+            if (_isDragging)
+            {
+                RotatePlanet();
+            }
+        }
+
+        private void RotatePlanet()
+        {
+            Vector3 currentMousePosition = Input.mousePosition;
+            Vector3 mouseDelta = currentMousePosition - _lastMousePosition;
+            
+            float rotationX = -mouseDelta.y * _rotationSpeed * Time.deltaTime;
+            float rotationY = mouseDelta.x * _rotationSpeed * Time.deltaTime;
+            
+            transform.Rotate(rotationX, rotationY, 0, Space.World);
+            
+            _lastMousePosition = currentMousePosition;
+        }
     }
 }
