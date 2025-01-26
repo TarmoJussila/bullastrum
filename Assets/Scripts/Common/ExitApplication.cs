229c067582828a59993a2b34701390da19e0c013
Tarmo Jussila <tarmo.jussila@gmail.com>
2025-01-26 15:06:51 +0200

    Exit button added

---
+++ Assets/Scripts/Common/ExitApplication.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Bullastrum.Common
+{
+    public class ExitApplication : MonoBehaviour
+    {
+        public void Exit()
+        {
+            Application.Quit();
+        }
+    }
+}
